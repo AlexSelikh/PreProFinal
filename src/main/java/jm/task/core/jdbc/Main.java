34c8c3f08ab05db59639fe8e9a29e7f716173f10
AlexSelikh <alselikhov@gmail.com>
2023-01-24 18:19:53 +0300

    Добавил в Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Alex", "1111", (byte) 1);
+        userService.saveUser("Alex2", "222", (byte) 2);
+        userService.saveUser("Alex3", "333", (byte) 3);
+        userService.saveUser("Alex4", "444", (byte) 4);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
